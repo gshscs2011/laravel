ecace3ff55af69ac340dce44261ed480379194c3
gshscs2011 <gshscs2011@gmail.com>
2019-12-20 10:31:16 +0900

    routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',[
+	'as' => 'home',
+	function (){
+		return 'my name "home" ';
+	}
+]);
+
+Route::get('/home', function(){
+	return redirect(route('home'));
 });
+
